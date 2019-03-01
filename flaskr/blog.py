9cf9dbc123c63ffae297e7e18263c45c49b19b88
Alaa Ayyash <aja5063@gmail.com>
2019-03-01 11:06:37 -0500

    will add base.html auth.py blog.py style.css

---
+++ flaskr/blog.py
@@ -0,0 +1,12 @@
+# Blog Blueprint
+
+# Define the blueprint and register it in the application factory
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from flaskr.auth import login_required
+from flaskr.db import get_db
+
+bp = Blueprint('blog', __name__)
